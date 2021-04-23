f973ee7b9a0d47288642e56e6f40ec26fb01bd83
ameen4455 <ameen4455@gmail.com>
2021-04-23 21:57:49 +0530

    added footer styling

--- src/components/EventFooter/EventFooter.js
+++ src/components/EventFooter/EventFooter.js
@@ -1,13 +1,49 @@
-import { Component } from 'react';
-import HeaderImage from '../../assets/HeaderImage.png';
-import './EventFooter.css';
+import "./EventFooter.css";
+import Logo from "../../assets/LogoWhite.png";
+import Twitter from "../../assets/twitter_blue.png";
+import Instagram from "../../assets/instagram_blue.png";
+import LinkedIn from "../../assets/linkedin_blue.png";
 
-export default class EventFooter extends Component {
-    render() {
-        return (
-           <div className="event-footer">
-                <div className="event-footer-margin"></div>
-           </div> 
-        )
-    }
-}
+const Footer = () => {
+    return (
+        <div className="event-footer">
+            <div className="event-footer-margin"></div>
+            <div className="container">
+                <div className="logo">
+                    <img src={Logo} alt="" />
+                </div>
+
+                <div className="description-container">
+                    <div className="description">
+                        <strong>
+                            Innovation and Entrepreneurship Development Cell{" "}
+                            <br />
+                        </strong>
+                        Govt. Model Engineering College <br />
+                        Thrikkakara, Kochi, Kerala 682021 <br /> iedc@mec.ac.in
+                    </div>
+                </div>
+
+                <div className={"socials"}>
+                    <a
+                        href="https://www.instagram.com/iedcmec"
+                        target="_blank "
+                    >
+                        <img src={Instagram} alt="" />
+                    </a>
+                    <a
+                        href="https://www.linkedin.com/company/iedcmec"
+                        target="_blank "
+                    >
+                        <img src={LinkedIn} alt="" />
+                    </a>
+                    <a href="https://www.twitter.com/iedc_mec" target="_blank ">
+                        <img src={Twitter} alt="" />
+                    </a>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
