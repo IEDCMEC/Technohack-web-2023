31b3324929a31ab00fb23678871dc5a3df654523
rindish krishna <rindishkrishna@gmail.com>
2021-04-24 10:25:26 +0530

    react router setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Component } from 'react';
-import {Route, BrowserRouter as Router,} from 'react-router-dom';
+import {Route, BrowserRouter,Switch} from 'react-router-dom';
 import './App.css';
 
 import Home from './pages/home';
@@ -8,11 +8,12 @@ import Event from './pages/event';
 export default class App extends Component {  
   render(){
     return(
-      <Router>
-        <Route path="/">
-          <Event />
-        </Route>
-      </Router> 
+      <BrowserRouter>
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/technohack" component={Event} exact />
+      </Switch>
+    </BrowserRouter>
     );
   }
 };
