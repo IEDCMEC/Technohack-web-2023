a7301e577bedf85369f8e736890b37bc21134379
Ashish <ashishmp0471@gmail.com>
2021-04-24 18:37:06 +0530

    Updated placeholder for upcoming speakers and events

--- src/components/Competitions/Competitions.js
+++ src/components/Competitions/Competitions.js
@@ -4,7 +4,7 @@ import './Competitions.css';
 export default class Competitions extends Component {
     render() {
         return (
-                <div className="competition">
+                <div className="competition" id="competition">
                     <div className="section-heading">Competition</div>
                     <div className="competition-container">
                         <div className="competition-box">
@@ -15,37 +15,41 @@ export default class Competitions extends Component {
                                 <div className="competition-button blue">Register</div>
                             </div>
                         </div>
-                        <div className="competition-box">
-                            <div className="competition-image"></div>
+                        <div className="competition-brewing-box">
+                            {/*<div className="competition-image"></div>
                             <div className="competition-text">
                                 <p className="competition-text-name">Etho Oral</p>
                                 <p className="competition-text-designation">Zerodha CEO</p>
                                 <div className="competition-button red">Register</div>
-                            </div>
+                            </div>*/}
+                            <p>Something's Brewing</p>
                         </div>
-                        <div className="competition-box">
-                            <div className="competition-image"></div>
-                            <div className="competition-text">
-                                <p className="competition-text-name">Etho Oral</p>
-                                <p className="competition-text-designation">Zerodha CEO</p>
-                                <div className="competition-button blue">Register</div>
-                            </div>
-                        </div>
-                        <div className="competition-box">
-                            <div className="competition-image"></div>
+                        <div className="competition-brewing-box">
+                            {/*<div className="competition-image"></div>
                             <div className="competition-text">
                                 <p className="competition-text-name">Etho Oral</p>
                                 <p className="competition-text-designation">Zerodha CEO</p>
                                 <div className="competition-button red">Register</div>
-                            </div>
+                            </div>*/}
+                            <p>Something's Brewing</p>
                         </div>
-                        <div className="competition-box">
-                            <div className="competition-image"></div>
+                        <div className="competition-brewing-box">
+                            {/*<div className="competition-image"></div>
                             <div className="competition-text">
                                 <p className="competition-text-name">Etho Oral</p>
                                 <p className="competition-text-designation">Zerodha CEO</p>
                                 <div className="competition-button red">Register</div>
-                            </div>
+                            </div>*/}
+                            <p>Something's Brewing</p>
+                        </div>
+                        <div className="competition-brewing-box">
+                            {/*<div className="competition-image"></div>
+                            <div className="competition-text">
+                                <p className="competition-text-name">Etho Oral</p>
+                                <p className="competition-text-designation">Zerodha CEO</p>
+                                <div className="competition-button red">Register</div>
+                            </div>*/}
+                            <p>Something's Brewing</p>
                         </div>
                     </div>
                 </div>
