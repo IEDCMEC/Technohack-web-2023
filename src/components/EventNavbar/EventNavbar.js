deec67634f53251e481dfecf1896f357abfdef21
Vishruth S <vs.vishruth.s@gmail.com>
2021-04-24 18:04:43 +0530

    Added navbar links to sections

--- src/components/EventNavbar/EventNavbar.js
+++ src/components/EventNavbar/EventNavbar.js
@@ -1,25 +1,40 @@
 import { Component } from 'react';
+import { NavHashLink as Link } from 'react-router-hash-link';
 import './EventNavbar.css';
 import logo from '../../assets/techno-logo.png'
+
 export default class EventNavbar extends Component {
     render() {
         return (<div className="nav-wrap">
             <div >
-            <img src={logo} alt="techno-hack" className="techno-logo-mobile"></img>
+                <img src={logo} alt="techno-hack" className="techno-logo-mobile"></img>
             </div>
-              
+
             <div className="event-navbar">
-                
-  <img src={logo} alt="techno-hack" className="techno-logo"></img>
-                
-              
-                <div className="event-nav-link event-active">Home</div>
-                <div className="event-nav-link ">Tracks</div>
-                <div className="event-nav-link">Prizes</div>
-                <div className="event-nav-link">Faq</div>
-                <div className="event-nav-link">Sponsors</div>
+                <img src={logo} alt="techno-hack" className="techno-logo"></img>
+                <div className="event-nav-link event-active"><a href="">Home</a></div>
+                <div className="event-nav-link ">
+                    <Link to="#problem-statement" smooth={true} spy={true} duration={1000}>
+                        Tracks
+                    </Link>
+                </div>
+                <div className="event-nav-link">
+                    <Link to="#prizes" smooth={true} spy={true} duration={1000}>
+                        Prizes
+                    </Link>
+                </div>
+                <div className="event-nav-link">
+                    <Link to="#faq" smooth={true} spy={true} duration={1000}>
+                        Faq
+                    </Link>
+                </div>
+                <div className="event-nav-link">
+                    <Link to="#sponsors" smooth={true} spy={true} duration={1000}>
+                        Sponsors
+                    </Link>
+                </div>
             </div>
-            </div>
+        </div>
         )
     }
-}    
+}
--- src/components/Speakers/Speakers.js
+++ src/components/Speakers/Speakers.js
@@ -4,46 +4,46 @@ import './Speakers.css';
 export default class Speakers extends Component {
     render() {
         return (
-                <div className="speaker">
-                    <div className="section-heading">Speaker Sessions</div>
-                    <div className="speaker-container">
-                        <div className="speaker-box">
-                            <div className="speaker-image"></div>
-                            <div className="speaker-text">
-                                <p className="speaker-text-name">Etho Oral</p>
-                                <p className="speaker-text-designation">Zerodha CEO</p>
-                            </div>
+            <div id="speakers" className="speaker">
+                <div className="section-heading">Speaker Sessions</div>
+                <div className="speaker-container">
+                    <div className="speaker-box">
+                        <div className="speaker-image"></div>
+                        <div className="speaker-text">
+                            <p className="speaker-text-name">Etho Oral</p>
+                            <p className="speaker-text-designation">Zerodha CEO</p>
                         </div>
-                        <div className="speaker-box">
-                            <div className="speaker-image"></div>
-                            <div className="speaker-text">
-                                <p className="speaker-text-name">Etho Oral</p>
-                                <p className="speaker-text-designation">Zerodha CEO</p>
-                            </div>
+                    </div>
+                    <div className="speaker-box">
+                        <div className="speaker-image"></div>
+                        <div className="speaker-text">
+                            <p className="speaker-text-name">Etho Oral</p>
+                            <p className="speaker-text-designation">Zerodha CEO</p>
                         </div>
-                        <div className="speaker-box">
-                            <div className="speaker-image"></div>
-                            <div className="speaker-text">
-                                <p className="speaker-text-name">Etho Oral</p>
-                                <p className="speaker-text-designation">Zerodha CEO</p>
-                            </div>
+                    </div>
+                    <div className="speaker-box">
+                        <div className="speaker-image"></div>
+                        <div className="speaker-text">
+                            <p className="speaker-text-name">Etho Oral</p>
+                            <p className="speaker-text-designation">Zerodha CEO</p>
                         </div>
-                        <div className="speaker-box">
-                            <div className="speaker-image"></div>
-                            <div className="speaker-text">
-                                <p className="speaker-text-name">Etho Oral</p>
-                                <p className="speaker-text-designation">Zerodha CEO</p>
-                            </div>
+                    </div>
+                    <div className="speaker-box">
+                        <div className="speaker-image"></div>
+                        <div className="speaker-text">
+                            <p className="speaker-text-name">Etho Oral</p>
+                            <p className="speaker-text-designation">Zerodha CEO</p>
                         </div>
-                        <div className="speaker-box">
-                            <div className="speaker-image"></div>
-                            <div className="speaker-text">
-                                <p className="speaker-text-name">Etho Oral</p>
-                                <p className="speaker-text-designation">Zerodha CEO</p>
-                            </div>
+                    </div>
+                    <div className="speaker-box">
+                        <div className="speaker-image"></div>
+                        <div className="speaker-text">
+                            <p className="speaker-text-name">Etho Oral</p>
+                            <p className="speaker-text-designation">Zerodha CEO</p>
                         </div>
                     </div>
                 </div>
-            )
-        }
-    };             
+            </div>
+        )
+    }
+};
