3415a6a1bc6951da6d1399cf7a56ca7d1856a5c7
Vivek R <123vivekr@gmail.com>
2021-04-25 22:07:03 +0530

    lots of things

--- src/components/EventFAQ/EventFaqData.js
+++ src/components/EventFAQ/EventFaqData.js
@@ -17,6 +17,12 @@ const eventData = [
     ans:
       "This event is completely free! We’ll provide you with a plethora of resources and tools to hack with.",
   },
+  {
+    id: 3,
+    question: "Who do I contact if I have any further doubts?",
+    ans:
+      "You can contact our Event Managers: Rindish (+91 85478 61324) or Adithya (+91 70127 03717). They'll be happy to help",
+  },
 ];
 
 export default eventData;
