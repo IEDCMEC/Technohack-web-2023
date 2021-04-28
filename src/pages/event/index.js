436753b4091e2340f9ddc14fceba2e1f4831ccbc
Vivek R <123vivekr@gmail.com>
2021-04-28 19:36:45 +0530

    add helmet

--- src/pages/event/index.js
+++ src/pages/event/index.js
@@ -7,6 +7,7 @@ import EventSponsers from '../../components/EventSponsers/EventSponsers'
 import EventFooter from '../../components/EventFooter/EventFooter';
 import BackgroundAnimation from '../../components/EventBackgroundAnimation/EventBackgroundAnimation';
 import './index.css';
+import { Helmet } from "react-helmet";
 
 export default function Event() {
     useEffect(() => {
@@ -16,13 +17,15 @@ export default function Event() {
 
         return (
            <div className="event-page">
-                <meta name="description" content="Here is the chance you’ve been waiting for to give wings to your creativity. Give life to your innovative ideas that can be solutions to the problems of today with TechnoHack!" />
-                <meta property="og:site_name" content="TechnoHack | Technopreneur 2021"/>
-                <meta property="og:title" content="TechnoHack | Technopreneur 2021" />
-                <meta property="og:description" content="Here is the chance you’ve been waiting for to give wings to your creativity. Give life to your innovative ideas that can be solutions to the problems of today with TechnoHack!" />
-                <meta property="og:image" itemprop="image" content="../../assets/technohacklogo.png"/>
-                <meta property="og:type" content="website" />
-                <link rel="apple-touch-icon" href="../../assets/technohacklogo.png" />
+               <Helmet>
+                    <meta name="description" content="Here is the chance you’ve been waiting for to give wings to your creativity. Give life to your innovative ideas that can be solutions to the problems of today with TechnoHack!" />
+                    <meta property="og:site_name" content="TechnoHack | Technopreneur 2021"/>
+                    <meta property="og:title" content="TechnoHack | Technopreneur 2021" />
+                    <meta property="og:description" content="Here is the chance you’ve been waiting for to give wings to your creativity. Give life to your innovative ideas that can be solutions to the problems of today with TechnoHack!" />
+                    <meta property="og:image" itemprop="image" content="../../assets/technohacklogo.png"/>
+                    <meta property="og:type" content="website" />
+                    <link rel="apple-touch-icon" href="../../assets/technohacklogo.png" />
+                </Helmet>
                 <EventHeader/>
                 <div className="event-background-container">
                     {/* <BackgroundAnimation /> */}
