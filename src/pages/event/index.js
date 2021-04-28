6d1d5673ad2740e0348df5ede32ecbb3346d5082
Vivek R <123vivekr@gmail.com>
2021-04-28 19:23:33 +0530

    technohack

--- src/pages/event/index.js
+++ src/pages/event/index.js
@@ -10,11 +10,19 @@ import './index.css';
 
 export default function Event() {
     useEffect(() => {
+        document.title = "TechnoHack | Technopreneur 2021"
         window.scrollTo(0,0);
       }, []);
 
         return (
            <div className="event-page">
+                <meta name="description" content="Here is the chance you’ve been waiting for to give wings to your creativity. Give life to your innovative ideas that can be solutions to the problems of today with TechnoHack!" />
+                <meta property="og:site_name" content="TechnoHack | Technopreneur 2021"/>
+                <meta property="og:title" content="TechnoHack | Technopreneur 2021" />
+                <meta property="og:description" content="Here is the chance you’ve been waiting for to give wings to your creativity. Give life to your innovative ideas that can be solutions to the problems of today with TechnoHack!" />
+                <meta property="og:image" itemprop="image" content="../../assets/technohacklogo.png"/>
+                <meta property="og:type" content="website" />
+                <link rel="apple-touch-icon" href="../../assets/technohacklogo.png" />
                 <EventHeader/>
                 <div className="event-background-container">
                     {/* <BackgroundAnimation /> */}
