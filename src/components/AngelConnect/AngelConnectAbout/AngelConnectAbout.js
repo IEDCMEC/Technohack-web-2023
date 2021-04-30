9c6d4039a197b90cff4784dd404582a155e8f75f
rachel1306 <60882090+rachel1306@users.noreply.github.com>
2021-04-30 09:29:03 +0530

    AngelConnect About us

--- src/components/AngelConnect/AngelConnectAbout/AngelConnectAbout.js
+++ src/components/AngelConnect/AngelConnectAbout/AngelConnectAbout.js
@@ -1,10 +1,24 @@
 import { Component } from "react";
 import './AngelConnectAbout.css';
-
+import border from '../../../assets/border.png';
+import logo from '../../../assets/angel_connect_white.png'
 export default class AngelConnectAbout extends Component {
     render() {
         return (
             <div className="angel-connect-about">
+              <h1 className='angel-heading'> ANGEL CONNECT </h1>
+              <p className='angel-text'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas proin dapibus orci quisque 
+              justo dui morbi. Id interdum eu nec at eros pretium.Lorem ipsum dolor sit amet, consectetur 
+              adipiscing elit. Maecenas proin dapibus orci quisque justo dui morbi. Id interdum eu nec at 
+              eros pretium.
+              <br /><br />
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas proin dapibus orci quisque 
+              justo dui morbi. Id interdum eu nec at eros pretium.Lorem ipsum dolor sit amet, consectetur 
+              adipiscing elit. Maecenas proin dapibus orci quisque justo dui morbi. Id interdum eu nec at 
+              eros pretium.
+              </p>
+               <img src={ logo } alt='AngelConnect' className='angelImg'/>
             </div>
           );
         }
