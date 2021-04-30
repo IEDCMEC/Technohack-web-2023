85cadb24d59251fa0fe942431803232f34a36e0c
Vivek R <123vivekr@gmail.com>
2021-04-30 15:15:36 +0530

    fix about page

--- src/components/AngelConnect/AngelConnectAbout/AngelConnectAbout.js
+++ src/components/AngelConnect/AngelConnectAbout/AngelConnectAbout.js
@@ -6,7 +6,7 @@ export default class AngelConnectAbout extends Component {
     render() {
         return (
             <div className="angel-connect-about">
-              <h1 className='angel-heading'> ANGEL CONNECT </h1>
+              <div className='angel-heading'> ANGEL CONNECT </div>
               <p className='angel-text'>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas proin dapibus orci quisque 
               justo dui morbi. Id interdum eu nec at eros pretium.Lorem ipsum dolor sit amet, consectetur 
