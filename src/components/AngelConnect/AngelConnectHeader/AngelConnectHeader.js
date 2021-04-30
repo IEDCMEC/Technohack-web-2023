bc5c2b19e83dd0eccaea75ace2ba0e3796556087
KhushiShah01 <khushi.bharat.shah@gmail.com>
2021-04-30 11:05:40 +0530

    angel connect logo -responsive and updated navbar

--- src/components/AngelConnect/AngelConnectHeader/AngelConnectHeader.js
+++ src/components/AngelConnect/AngelConnectHeader/AngelConnectHeader.js
@@ -15,13 +15,13 @@ export default class AngelConnectHeader extends Component {
                 <AngelConnectNavbar />
                 <div className="container" >
                     <div className="angelconnect">
-                        <img src={title} alt="Angel Connect" ></img>
+                    <img src={title} alt="Angel Connect" width='412' height='232'></img>
                     </div>
                     <div className="content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam quis velit augue. Sed ornare massa lobortis luctus vehicula. Donec est odio, venenatis sit amet neque eu, pulvinar commodo sapien.</div>
                     <div className="register">
                         <Link className="register" to="#" smooth={true} >
                             REGISTER
-                    </Link>
+                        </Link>
                     </div>
                 </div>
             </div>
--- src/components/AngelConnect/AngelConnectNavbar/AngelConnectNavbar.js
+++ src/components/AngelConnect/AngelConnectNavbar/AngelConnectNavbar.js
@@ -1,53 +1,40 @@
 import { Component } from 'react';
 import { NavHashLink as Link } from 'react-router-hash-link';
 import './AngelConnectNavbar.css';
-import techno from '../../../assets/Main logo.png';
-import menulogo from '../../../assets/menu logo.png';
+import logo from '../../../assets/Main logo.png'
 
 export default class EventNavbar extends Component {
     render() {
-        return (
-            <div className="nav-wrap">
-                <div className="angel-connect-navbar">
-
-                    <img src={techno} alt="techno-hack" className="techno-logo"></img>
-
-                    { /* <div className="angel-connect-nav-link angel-connect-active"><a href="">Home</a></div>  */}
-
-                    
-                    <div className="angel-connect-nav-link ">
-                        <Link to="#problem-statement-link_for_navbar" smooth={true} spy={true} duration={1000}>
-                            HOME
-                            </Link>
-                    </div>
-                    <div className="angel-connect-nav-link" >
-                        <Link to="#prizes-link_for_navbar" smooth={true} spy={true} duration={1000}>
-                            ANGEL CONNECT
-                            </Link>
-                    </div>
-                    <div className="angel-connect-nav-link">
-                        <Link to="#angelconnecttimeline" smooth={true} spy={true} duration={1000}>
-                            TIMELINE
-                             </Link>
-                    </div>
-                    <div className="angel-connect-nav-link">
-                        <Link to="#angelconnectinvestors" smooth={true} spy={true} duration={1000}>
-                            INVESTORS
-                            </Link>
-                    </div>
-                    <div className="angel-connect-nav-link">
-                        <Link to="#sponsors-link_for_navbar" smooth={true} spy={true} duration={1000}>
-                            FAQ
-                            </Link>
-                    </div>
-                    <div className="angel-connect-nav-link">
-                        <Link to="#sponsors-link_for_navbar" smooth={true} spy={true} duration={1000}>
-                            CONTACT US
-                            </Link>
-                    </div>
-                </div>
+        return (<div className="nav-wrap">
+            <div >
+                <img src={logo} alt="techno-hack" className="techno-logo-mobile"></img>
             </div>
 
+            <div className="angel-connect-navbar">
+                <img src={logo} alt="techno-hack" className="techno-logo"></img>
+                {/* <div className="angel-connect-nav-link angel-connect-active"><a href="">Home</a></div> */}
+                <div className="angel-connect-nav-link ">
+                    <Link to="#problem-statement-link_for_navbar" smooth={true} spy={true} duration={1000}>
+                        Tracks
+                    </Link>
+                </div>
+                <div className="angel-connect-nav-link">
+                    <Link to="#prizes-link_for_navbar" smooth={true} spy={true} duration={1000}>
+                        Prizes
+                    </Link>
+                </div>
+                <div className="angel-connect-nav-link">
+                    <Link to="#faq-link_for_navbar_two" smooth={true} spy={true} duration={1000}>
+                        Faq
+                    </Link>
+                </div>
+                <div className="angel-connect-nav-link">
+                    <Link to="#sponsors-link_for_navbar" smooth={true} spy={true} duration={1000}>
+                        Sponsors
+                    </Link>
+                </div>
+            </div>
+        </div>
         )
     }
 }
