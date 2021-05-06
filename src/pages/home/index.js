df33388f3887262293632b373c19e2f07c4ae2cb
Ashish <ashishmp0471@gmail.com>
2021-05-06 22:43:10 +0530

    Removed absolute positioning for modal content

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,14 +2,9 @@ import { Component } from "react";
 import Header from "../../components/Home/Header/Header";
 import Speakers from "../../components/Home/Speakers/Speakers";
 import Competitions from "../../components/Home/Competitions/Competitions";
-<<<<<<< HEAD
-import Workshops from "../../components/Home/Workshops/Workshops"
-import Faq from "../../components/Faq/Faq";
-import data from "../../components/Faq/FaqData";
-=======
->>>>>>> 3a196d8c6bf407941cd002efffe7350474b583ef
 import Footer from "../../components/Home/Footer/Footer";
 import About from "../../components/Home/About/About";
+import Workshops from "../../components/Home/Workshops/Workshops"
 import BackgroundAnimation from "../../components/Home/BackgroundAnimation/BackgroundAnimation";
 import HomeFaq from "../../components/Home/HomeFaq/HomeFaq";
 export default class Home extends Component {
