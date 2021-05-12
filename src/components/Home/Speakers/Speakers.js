281a71fe93409f8e364d42ff0f260a123a9e1ff7
Vishruth S <vs.vishruth.s@gmail.com>
2021-05-12 13:04:22 +0530

    speakers responsive bug fix

--- src/components/Home/Speakers/Speakers.js
+++ src/components/Home/Speakers/Speakers.js
@@ -19,11 +19,11 @@ const responsive = {
         items: 3,
     },
     tablet: {
-        breakpoint: { max: 1024, min: 464 },
+        breakpoint: { max: 1024, min: 764 },
         items: 2,
     },
     mobile: {
-        breakpoint: { max: 464, min: 0 },
+        breakpoint: { max: 764, min: 0 },
         items: 1,
     }
 };
