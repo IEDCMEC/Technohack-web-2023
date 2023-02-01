13b42e8f884d127bbb2bea20a31ae08973d4035d
adithyaanilkumar <adithyaanilkumar1@gmail.com>
2023-02-01 14:54:51 +0530

    final edits

---
+++ src/components/Event/EventCommunity/EventCommunity.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import EventSectionHeading from '../EventSectionHeading/EventSectionHeading'
+export default function EventCommunity() {
+  return (
+    <div>
+        <EventSectionHeading text="Community Partners"/>
+        <div className="sponsors-container">
+                <h3 style={{marginBottom:''}}>To be Announced ...</h3></div>
+    </div>
+  )
+}
