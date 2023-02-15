69c44b7cbc8c3e1c4e5512ee24318e818a37125f
Aldrin Jenson <aldrinjenson@gmail.com>
2023-02-15 22:23:08 +0530

    Fix issue with coc page

--- src/pages/coc/index.js
+++ src/pages/coc/index.js
@@ -1,9 +1,11 @@
-import { Redirect } from "react-router";
+import { useEffect } from "react";
 
 const CoC = () => {
-  return (
-    <Redirect to='https://technohack.notion.site/Code-Of-Conduct-7f4698ca44114a3596d08fa82fa55075' />
-  );
+  useEffect(() => {
+    window.location.href =
+      "https://technohack.notion.site/Code-Of-Conduct-7f4698ca44114a3596d08fa82fa55075";
+  }, []);
+  return <div></div>;
 };
 
 export default CoC;
