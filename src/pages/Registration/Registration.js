f1bb6e99617f2fd86d5d56c43b8b85e14890d567
Arjun J <arjunjins2001@gmail.com>
2023-02-19 20:54:19 +0530

    formik initial setup

--- src/pages/Registration/Registration.js
+++ src/pages/Registration/Registration.js
@@ -1,48 +1,80 @@
-import React, { useState } from "react";
+import { Formik } from "formik";
+import React from "react";
+import * as Yup from "yup";
 
 const Registration = () => {
-  const [formData, setFormData] = useState({
+  const initialValue = {
     firstName: "",
     lastName: "",
     email: "",
-    comments: "",
+  };
+
+  const validation = Yup.object({
+    firstName: Yup.string().required("Please add a valid project name").min(3),
+    lastName: Yup.string().required("Please add a valid project name").min(3),
+    email: Yup.string()
+      .email("Invalid email address")
+      .required("Please enter a valid email ID"),
   });
 
-  function handleChange(event) {
-    setFormData((prevFormData) => {
-      return {
-        ...prevFormData,
-        [event.target.name]: event.target.value,
-      };
-    });
-  }
-  console.log(formData.email);
+  const handleSubmit = (values) => {
+    alert(JSON.stringify(values, null, 2));
+  };
   return (
-    <div>
-      <form onSubmit="">
-        <input
-          type="text"
-          name="firstName"
-          value={formData.firstName}
-          placeholder="First Name"
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          name="lastName"
-          value={formData.lastName}
-          placeholder="Last Name"
-          onChange={handleChange}
-        />
-        <input
-          type="email"
-          name="email"
-          value={formData.email}
-          placeholder="abc@def.com"
-          onChange={handleChange}
-        />
-      </form>
-    </div>
+    <>
+      <Formik
+        initialValues={initialValue}
+        validationSchema={validation}
+        onSubmit={handleSubmit}
+      >
+        {(formik) => (
+          <div>
+            <form onSubmit={formik.handleSubmit}>
+              <label htmlFor="firstName">First Name</label>
+              <input
+                type="text"
+                name="firstName"
+                id="firstName"
+                value={formik.values.firstName}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+              />
+              {formik.touched.firstName && formik.errors.firstName ? (
+                <div>{formik.errors.firstName}</div>
+              ) : null}
+
+              <label htmlFor="lastName">Last Name</label>
+              <input
+                type="text"
+                name="lastName"
+                id="lastName"
+                value={formik.values.lastName}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+              />
+              {formik.touched.lastName && formik.errors.lastName ? (
+                <div>{formik.errors.lastName}</div>
+              ) : null}
+
+              <label htmlFor="email">Email Address</label>
+              <input
+                type="text"
+                name="email"
+                id="email"
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+              />
+              {formik.touched.email && formik.errors.email ? (
+                <div>{formik.errors.email}</div>
+              ) : null}
+
+              <button type="submit">Submit</button>
+            </form>
+          </div>
+        )}
+      </Formik>
+    </>
   );
 };
 
