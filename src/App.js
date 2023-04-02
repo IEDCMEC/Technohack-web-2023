b40b1beb8b4abddf38e3e439bdbcfffd5252b3fc
Aldrin Jenson <aldrinjenson@gmail.com>
2023-04-02 18:27:20 +0530

    minor refactoring

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-import { Component, useEffect, useState } from "react";
+import { Component } from "react";
 import "./App.css";
 import { Route, BrowserRouter, Switch } from "react-router-dom";
 import Event from "./pages/event";
 import CoC from "./pages/coc";
-import Registration from "./pages/Registration/Registration";
-// import {Event,CoC,Registration} from './pages/index'
+// import Registration from "./pages/Registration/Registration";
 import { Toaster } from "react-hot-toast";
 export default class App extends Component {
   render() {
-
-
     return (
       <BrowserRouter>
         <Toaster />
         <Switch>
-          <Route path="/" component={Event} exact />
-          <Route path="/code-of-conduct" component={CoC} exact />
+          <Route path='/' component={Event} exact />
+          <Route path='/code-of-conduct' component={CoC} exact />
           {/* <Route path="/register" component={Registration} exact /> */}
         </Switch>
       </BrowserRouter>
